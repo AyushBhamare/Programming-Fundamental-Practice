10b140d620d9580a1b6b468de34f6d0caa1ff27a
AyushBhamare <ayushbhamare99@gmail.com>
2026-04-25 00:07:17 +0530

    Add C program demonstrating function call to display message

---
+++ C/Basics/function_display_message.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// This program demonstrates a simple function call in C
+// It prints a greeting message
+
+void Display()
+{
+    printf("Jay Ganesh...\n");
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
